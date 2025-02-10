5d2a01e52e3f8b2fb309cb0a1ccc40eb28c38ba2
Tahsinqureshii <m.tahsinqureshi11@gmail.com>
2025-02-10 15:12:39 +0530

    PaymentModuleAdded

--- src/com/mind/payment/PaymentProcessor.java
+++
@@ -1,9 +0,0 @@
-package com.mind.payment;
-
-public class PaymentProcessor {
-
-	  public void processPayment(PaymentMethod paymentMethod, double amount) {
-	        paymentMethod.pay(amount);  
-	    }
-	
-}
